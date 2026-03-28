939b4975343fa6efac8f67d116da0fe59f218782
Josh Wright <wright.jjw@gmail.com>
2026-03-28 12:43:36 -0400

    parser mmai-s test

---
+++ src/parser.ts
@@ -0,0 +1,13 @@
+import type Character from "./types/character";
+
+/**
+ * Contains functions for parsing user input.
+ */
+export class Parser {
+  /**
+   * Parse input from $mmai-s and convert it into a character list.
+   */
+  static parseSeriesImages(input: string): Character[] {
+    throw "not implemented";
+  }
+}
