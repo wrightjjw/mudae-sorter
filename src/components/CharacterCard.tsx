1569ef6423ea27189870864d51df186ee11de174
Josh Wright <wright.jjw@gmail.com>
2026-04-29 00:42:48 -0400

    adjust sizes on cards

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,9 +1,9 @@
 import type Character from "../types/character";
 
-export default function Sorter({ character }: { character: Character }) {
+export default function CharacterCard({ character }: { character: Character }) {
 
     return (
-        <div>
+        <div className="card">
             <img src={character.imageUrl} referrerPolicy="no-referrer" />
         </div>
     )
